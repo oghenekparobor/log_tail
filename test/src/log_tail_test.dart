224a1a2519b9308d2a07a3cc30e7e7e2033cfcaf
Oghenekparobor Eminokanju <robor.eminokanju@gmail.com>
2024-09-06 15:29:41 +0100

    chore: increased test coverage

--- test/src/log_tail_test.dart
+++ test/src/log_tail_test.dart
@@ -3,17 +3,23 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:log_tail/log_tail.dart';
 import 'package:log_tail/src/client/log_tail.client.dart';
+import 'package:logger/logger.dart';
 import 'package:mocktail/mocktail.dart';
 
 class MockLogTailClient extends Mock implements LogTailClient {}
+
+class MockLogger extends Mock implements Logger {}
 
 void main() {
   late MockLogTailClient client;
   const sourceToken = '12345_12323';
   late LogTail logTail;
+  late MockLogger logger;
 
   setUp(() {
     client = MockLogTailClient();
+    logger = MockLogger();
+
     logTail = LogTail(sourceToken, client: client);
   });
 
@@ -21,6 +27,8 @@ void main() {
     test('log single event', () async {
       // Arrange
       const response = 'The event(s) were successfully logged';
+
+      when(() => logger.d('event')).thenReturn(null);
 
       when(() => client.sendSingleEvent(event: 'event')).thenAnswer(
         (_) async => response,
